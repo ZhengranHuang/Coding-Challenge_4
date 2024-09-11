abd048d1eaa94777ab150fe28327169b323d99cb
ZhengranHuang <Zhengran@usf.edu>
2024-09-11 13:07:30 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -1,4 +1,5 @@
 //Task 1:Create an Inventory Array of Product Objects
+
 const inventory = [
 { name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
 { name: 'Smartphone', price: 800, quantity: 5, lowStockLevel: 2 },
@@ -6,3 +7,23 @@ const inventory = [
 { name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
 { name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 1 }
 ];
+
+//Task 2:Create a Function to Display Product Details
+
+function displayProductDetails(product) {
+    let stocklevel;
+    
+    if (product.quantity <= product.lowStockLevel) {
+      stocklevel = 'Low Stock';
+    } else if (product.quantity > product.lowStockLevel){
+      stocklevel = 'In Stock';
+    }
+// use if to check the stock status.
+console.log(`Product: ${product.name}`);
+console.log(`Price: $${product.price0}`);
+console.log(`Quantity in Stock: ${product.quantity}`);
+console.log(`Stock level: ${stocklevel}`);
+//accepts a product object and logs the product’s name, price, quantity in stock, and stock status
+}
+//check the output
+inventory.forEach(displayProductDetails);
